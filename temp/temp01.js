84cc3203947333923cedc4c28d8bcad6eb936b09
Tan CS <tan_chee_seong@sp.edu.sg>
2026-04-27 13:39:15 +0800

    temp example updated

---
+++ temp/temp01.js
@@ -0,0 +1,8 @@
+function printME(s) {
+    console.log("1. " + s);
+    console.log("2. " + s);
+    return s;
+    console.log("3. " + s);
+}
+
+printME("Hello");
---
+++ temp/temp02.js
@@ -0,0 +1,19 @@
+console.clear();
+
+function changeX() {
+  let x = 10;
+}
+
+function changeY() {
+  y = 12;
+}
+
+let x = 5;
+let y = 6;
+
+changeX();
+changeY();
+
+console.log(`x is ${x}`);
+console.log(`y is ${y}`);
+
